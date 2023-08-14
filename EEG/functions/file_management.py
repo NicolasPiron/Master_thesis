5de6be6fa6aab1e7afdc8cc18f4b29cd1504e776
NicolasPiron <ncls.piron@gmail.com>
2023-08-14 15:29:22 +0200

    fixed bug + changed path organisation + added population parameter

--- EEG/functions/file_management.py
+++ EEG/functions/file_management.py
@@ -2,13 +2,15 @@ import glob
 import os
 import mne
 
-def concat_all_subj(task='n2pc'):
+def concat_all_subj(task='n2pc', population='control'):
     ''' Concatenate the files of all the subjects that have been preprocessed.
     
     Parameters
     ---------- 
     task : 'alpheye' or 'n2pc'
         The name of the task
+    population : 'control' or 'patient'
+        The type of population
     
     Returns
     ----------
@@ -17,33 +19,59 @@ def concat_all_subj(task='n2pc'):
 
     if task == 'alpheye':
 
-        data_path = '/Users/nicolaspiron/Documents/PULSATION/Python_MNE/preproc/Alpheye_out/data/epochs/'
-        out_path = '/Users/nicolaspiron/Documents/PULSATION/Python_MNE/preproc/Alpheye_out/data/all_subj/'
+        data_path = '/Users/nicolaspiron/Documents/PULSATION/Data/EEG/Alpheye/data-preproc-alpheye/total_epochs/'
+        out_path = '/Users/nicolaspiron/Documents/PULSATION/Data/EEG/Alpheye/data-preproc-alpheye/epochs_all/'
+        
         files = glob.glob(data_path+'*')
         files.sort()
         total_epoch_files = []
 
-        for i, file in enumerate(files):
-            if 'total' in file:
-                i = mne.read_epochs(file)
-                total_epoch_files.append(i)
+        if population == 'control':
 
-        all_subj = mne.concatenate_epochs(total_epoch_files)
-        all_subj.save(os.path.join(out_path,'all_subj_alpheye_.fif'), overwrite=True)
+            for file in files:
+                if data_path + 'S' in file:
+                    i = mne.read_epochs(file)
+                    total_epoch_files.append(i)
+        
+            all_subj = mne.concatenate_epochs(total_epoch_files)
+            all_subj.save(os.path.join(out_path,'S_all_subj_alpheye_.fif'), overwrite=True)
+        
+        elif population == 'patient':
+
+            for file in files:
+                if data_path + 'P' in file:
+                    i = mne.read_epochs(file)
+                    total_epoch_files.append(i)
+
+            all_subj = mne.concatenate_epochs(total_epoch_files)
+            all_subj.save(os.path.join(out_path,'P_all_subj_alpheye_.fif'), overwrite=True)
 
 
     elif task == 'n2pc':
 
-        data_path = '/Users/nicolaspiron/Documents/PULSATION/Python_MNE/preproc/n2pc_out/data/epochs/'
-        out_path = '/Users/nicolaspiron/Documents/PULSATION/Python_MNE/preproc/n2pc_out/data/all_subj/'
+        data_path = '/Users/nicolaspiron/Documents/PULSATION/Data/EEG/N2pc/data-preproc-n2pc/total_epochs/'
+        out_path = '/Users/nicolaspiron/Documents/PULSATION/Data/EEG/N2pc/data-preproc-n2pc/epochs_all/'
+        
         files = glob.glob(data_path+'*')
         files.sort()
         total_epoch_files = []
 
-        for i, file in enumerate(files):
-            if 'total' in file:
-                i = mne.read_epochs(file)
-                total_epoch_files.append(i)
+        if population == 'control':
+
+            for file in files:
+                if data_path + 'S' in file:
+                    i = mne.read_epochs(file)
+                    total_epoch_files.append(i)
+                        
+            all_subj = mne.concatenate_epochs(total_epoch_files)
+            all_subj.save(os.path.join(out_path,'S_all_subj_n2pc_.fif'), overwrite=True)                        
+        
+        elif population == 'patient':
+
+            for file in files:
+                if data_path + 'P' in file:
+                    i = mne.read_epochs(file)
+                    total_epoch_files.append(i)
 
         all_subj = mne.concatenate_epochs(total_epoch_files)
-        all_subj.save(os.path.join(out_path,'all_subj_n2pc_.fif'), overwrite=True)
+        all_subj.save(os.path.join(out_path,'P_all_subj_n2pc_.fif'), overwrite=True)
