e8728abc06c92aefac3f58ff82621e3fa9b2b74f
NicolasPiron <ncls.piron@gmail.com>
2023-09-10 18:01:13 +0200

    usable to preproc resting state data

--- EEG/functions/preproc.py
+++ EEG/functions/preproc.py
@@ -8,16 +8,16 @@ import pandas as pd
 import autoreject
 
 def load_data(subject_id, task, input_path, plot_data=True):
-    ''' Loads the data from the BIDS folder and concatenates the two runs.
+    ''' Loads the data from the BIDS folder and concatenates the runs if there is more than 1 run.
 
     Parameters
     ----------
     subject_id : str
-        The subject ID.
+        The subject ID. 2 digits.
     task : str
-        The task name. Default is 'N2pc'.
-    input_path : strs
-        The path to the BIDS folder. Default is '/Users/nicolaspiron/Documents/Master_thesis/EEG/toBIDS/BIDS_data/sourcedata/'.
+        The task name. Can be 'N2pc', 'Alpheye', 'RESTINGSTATEOPEN' or 'RESTINGSTATECLOSED'.
+    input_path : str
+        The path to the BIDS folder (sourcedata).
     plot_data : bool
         Whether to plot the raw data or not. Default is True.
     
@@ -25,39 +25,58 @@ def load_data(subject_id, task, input_path, plot_data=True):
     -------
     raw : mne.io.Raw
         The raw data.
-    e_list : array
+    e_list : array : only for N2pc and Alpheye (no events for resting state)
         The event list.
     '''
-    subject_id = str(subject_id)
-    path = fr"sub-{subject_id}/ses-01/eeg/sub-{subject_id}_ses-01_task-{task}_run-"
 
-    # Load the data and concatenate the two runs
-    files = glob.glob(os.path.join(input_path,path+'*.bdf'))
-    files.sort()
-    
-    # Other option / should work with both tasks
-    raws = []
-    for file in files:
+    subject_id = str(subject_id)
+
+    if task == 'RESTINGOPEN' or 'RESTINGCLOSE':
+
+        file = os.path.join(input_path, fr"sub-{subject_id}/ses-01/eeg/sub-{subject_id}_ses-01_task-{task}_eeg.bdf")
+
         raw = mne.io.read_raw_bdf(file)
         raw.load_data()
-        raws.append(raw)
-    raw = mne.concatenate_raws(raws)
 
-    # Find the events and create annotations
-    e_list = mne.find_events(raw, stim_channel='Status')
-    annot = mne.annotations_from_events(e_list, raw.info['sfreq'])
-    raw = raw.set_annotations(annot)
+        # Plot the raw data to select bad channels
+        if plot_data == True:
+            raw.plot(scalings='auto')
 
-    # Plot the raw data to select bad channels
-    if plot_data == True:
-        raw.plot(scalings='auto')
+            # Pause the script until you press a key
+            plt.show(block=False)
+            input("Press Enter when all the bad channels have been defined...")
+            print('====================== Bad channels defined')
 
-        # Pause the script until you press a key
-        plt.show(block=False)
-        input("Press Enter when all the bad channels have been defined...")
-        print('====================== Bad channels defined')
+        return raw
 
-    return raw, e_list
+    else:
+
+        path = fr"sub-{subject_id}/ses-01/eeg/sub-{subject_id}_ses-01_task-{task}_run-"
+        files = glob.glob(os.path.join(input_path,path+'*.bdf'))
+        files.sort()
+        
+        # Other option / should work with both tasks
+        raws = []
+        for file in files:
+            raw = mne.io.read_raw_bdf(file)
+            raw.load_data()
+            raws.append(raw)
+        raw = mne.concatenate_raws(raws)
+
+        e_list = mne.find_events(raw, stim_channel='Status')
+        annot = mne.annotations_from_events(e_list, raw.info['sfreq'])
+        raw = raw.set_annotations(annot)
+
+        # Plot the raw data to select bad channels
+        if plot_data == True:
+            raw.plot(scalings='auto')
+
+            # Pause the script until you press a key
+            plt.show(block=False)
+            input("Press Enter when all the bad channels have been defined...")
+            print('====================== Bad channels defined')
+
+        return raw, e_list
 
 def filter_and_interpolate(subject_id, task, raw, output_path, plot_data=True):
     ''' Drops useless channels, sets the channel types and the montage, filters the data and interpolates bad channels. 
@@ -159,42 +178,64 @@ def epoch_data(subject_id, task, raw, e_list,  output_path):
         df = pd.DataFrame(e_list, columns=['timepoint', 'duration', 'stim'])
         mask = (df['stim'].isin(range(1, 9))) & (df['stim'].shift(-1) == 128) | (df['stim'] == 128) & (df['stim'].shift(1).isin(range(1,9)))
         correct_df = df[mask]
-        mne_events = correct_df.values
+        events = correct_df.values
 
-        event_dict = {'dis_top/target_l':1,
-              'dis_top/target_r':2,
-              'no_dis/target_l':3,
-              'no_dis/target_r':4,
-              'dis_bot/target_l':5,
-              'dis_bot/target_r':6,
-              'dis_right/target_l':7,
-              'dis_left/target_r':8,
-             }
+        event_id = {'dis_top/target_l':1,
+            'dis_top/target_r':2,
+            'no_dis/target_l':3,
+            'no_dis/target_r':4,
+            'dis_bot/target_l':5,
+            'dis_bot/target_r':6,
+            'dis_right/target_l':7,
+            'dis_left/target_r':8,
+            }
         
-        # Epoch the data
-        epochs = mne.Epochs(raw, mne_events, event_id=event_dict, tmin=-0.2, tmax=0.8, baseline=None, event_repeated='drop', preload=True)
-        
+        tmin = -0.2
+        tmax = 0.8
+
     elif task == 'Alpheye':
-        
+
         df = pd.DataFrame(e_list, columns=['timepoint', 'duration', 'stim'])
         df_stim = df[(df['stim'] == 2) | (df['stim'] == 4)].reset_index()
         df_stim = df_stim.add_prefix('img_')
-        mne_events = df_stim[['img_timepoint', 'img_duration', 'img_stim']].values
-        
-        event_dict = {'Landscape':2,
-                    'Human':4}
+        events = df_stim[['img_timepoint', 'img_duration', 'img_stim']].values
 
-        epochs = mne.Epochs(raw, mne_events, event_id=event_dict, tmin=-0.2, tmax=6, baseline=(-0.2, 0), event_repeated='drop', preload=True)
+        event_id = {'Landscape':2,
+                    'Human':4}
+        tmin = -0.2
+        tmax = 6.0
+    
+    elif task == 'RESTINGSTATECLOSE' or 'RESTINGSTATEOPEN':
+        
+        tmin = 0
+        tmax = 2.0
+        
+        # Define the sampling rate and epoch duration in seconds
+        sampling_rate = raw.info['sfreq']
+        epoch_duration = tmax - tmin
+        
+        # Create fake events at regular intervals
+        fake_event_samples = np.arange(0, len(raw), int(epoch_duration * sampling_rate))
+
+        events = np.array([fake_event_samples, np.zeros_like(fake_event_samples), np.ones_like(fake_event_samples)]).T
+        event_id = {'fake_event':1}
+        
+        # even if not very useful, transform the fake event list into a df to save it
+        df = pd.DataFrame({'timepoint':events[:,0], 'duration':events[:,1], 'stim':events[:,2]})
+    
+    # Epoch the data
+    epochs = mne.Epochs(raw, events=events, event_id=event_id, tmin=tmin, tmax=tmax, baseline=None, event_repeated='drop', preload=True)
 
     # Set the EEG reference and resample the data
     epochs.set_eeg_reference(ref_channels='average')
     epochs.resample(512)
-    
+
     # Save the event list and the epochs
-    if os.path.exists(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'step-03-event_lists')) == False:
-        os.makedirs(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'step-03-event_lists'))
-        print('Directory created')
-    df.to_csv(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'step-03-event_lists', f'sub-{subject_id}-elist-{task}.csv'), index=False)
+    if task == 'N2pc' or 'Alpheye':
+        if os.path.exists(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'step-03-event_lists')) == False:
+            os.makedirs(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'step-03-event_lists'))
+            print('Directory created')
+        df.to_csv(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'step-03-event_lists', f'sub-{subject_id}-elist-{task}.csv'), index=False)
 
     if os.path.exists(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'step-04-epochs')) == False:
         os.makedirs(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'step-04-epochs'))
@@ -307,8 +348,9 @@ def automated_epochs_rejection(subject_id, task, epochs, output_path):
     ar_epochs, final_reject_log = ar.fit_transform(epochs, return_log=True)
 
 
-    # Apply baseline correction AFTER ICA
-    epochs_clean = epochs_clean.apply_baseline(baseline=(-0.2,0), verbose=True)
+    if task == 'N2pc':
+        # Apply baseline correction AFTER ICA
+        epochs_clean = epochs_clean.apply_baseline(baseline=(-0.2,0), verbose=True)
 
     # Plot the average signal before and after rejecting the epochs and save the plot
     evoked_bad = epochs[final_reject_log.bad_epochs].average()
@@ -402,6 +444,13 @@ def quality_check_plots(subject_id, task, epochs, epochs_clean, output_path):
         human_epochs = epochs_clean["Human"]
         human_evoked = human_epochs.average()
         evoked_topo = human_evoked.plot_topomap(times=[0.0, 0.2, 0.5, 1, 2, 3], ch_type="eeg")
+
+    elif task == 'RESTINGSTATECLOSE' or 'RESTINGSTATEOPEN':
+
+        # Topo for resting state
+        resting_epochs = epochs_clean["fake_event"]
+        resting_evoked = resting_epochs.average()
+        evoked_topo = resting_evoked.plot_topomap(times=[0.0, 0.5, 1.0, 1.5, 1.9], ch_type="eeg")
     
     # Save the plots
     if os.path.exists(os.path.join(output_path, f'sub-{subject_id}', 'preprocessing', 'plots', 'step-06-evoked_topo')) == False:
--- EEG/preproc_pipeline.py
+++ EEG/preproc_pipeline.py
@@ -14,17 +14,26 @@ def preproc_pipeline(subject_id):
     None
     '''
     # Define task
-    task = 'N2pc'
+    task = 'RESTINGSTATECLOSE'
     # Path to data
-    input_path = '/home/nicolasp/local_PULSATION/Master_thesis/EEG/toBIDS/BIDS_data/sourcedata'
+    input_path = '/Users/nicolaspiron/Documents/Master_thesis/EEG/toBIDS/BIDS_data/sourcedata'
     # Where the output files are saved
-    output_path = '/home/nicolasp/shared_PULSATION/derivative'
+    output_path = '/Users/nicolaspiron/Documents/PULSATION/Python_MNE/output_preproc'
     # Load data
-    raw, e_list = pp.load_data(subject_id, task, input_path, plot_data=True)
-    # Filter and interpolate
-    raw = pp.filter_and_interpolate(subject_id=subject_id, task=task, raw=raw, output_path=output_path, plot_data=False)
-    # Epoch data
-    epochs = pp.epoch_data(subject_id=subject_id, task=task, raw=raw, e_list=e_list, output_path=output_path)
+    if task == 'RESTINGSTATECLOSE' or task == 'RESTINGSTATEOPEN':
+        raw = pp.load_data(subject_id, task, input_path, plot_data=True)
+        # Filter and interpolate
+        raw = pp.filter_and_interpolate(subject_id=subject_id, task=task, raw=raw, output_path=output_path, plot_data=False)
+        # Epoch data
+        epochs = pp.epoch_data(subject_id=subject_id, task=task, raw=raw, e_list=None, output_path=output_path)
+
+    else:
+        raw, e_list = pp.load_data(subject_id, task, input_path, plot_data=True)
+        # Filter and interpolate
+        raw = pp.filter_and_interpolate(subject_id=subject_id, task=task, raw=raw, output_path=output_path, plot_data=False)
+        # Epoch data
+        epochs = pp.epoch_data(subject_id=subject_id, task=task, raw=raw, e_list=e_list, output_path=output_path)
+
     # Apply ICA and reject bad epochs
     epochs_clean = pp.automated_epochs_rejection(subject_id=subject_id, task=task, epochs=epochs, output_path=output_path)
     # Plot cleaned data
