be7f1d9a01af4529f4c1aae19f4aacaad902701f
NicolasPiron <ncls.piron@gmail.com>
2023-09-28 16:56:01 +0200

    new file to use the new functions (plot topo)

---
+++ EEG/plot_topography.py
@@ -0,0 +1,71 @@
+import functions.ERP as erp
+import argparse
+
+
+
+# Path to data
+input_dir = '/Users/nicolaspiron/Documents/PULSATION/Python_MNE/output_preproc'
+# Where the output files are saved
+output_dir = '/Users/nicolaspiron/Documents/PULSATION/Python_MNE/output_preproc'
+# Population (control or stroke)
+population = 'control'
+
+# Subject list when analysing single subjects
+subject_list = [1]
+
+# List of subjects to be excluded from the grand average
+excluded_subjects_list = [4, 1, 2, 3]
+
+##############################################################################################################
+
+def loop_over_subjects_topo(subject_list, input_dir, output_dir):
+
+   for subject in subject_list:
+        subject_id = str(subject)
+        if len(subject_id) == 1:
+            subject_id = '0' + subject_id
+
+        erp.plot_erp_topo(subject_id, input_dir, output_dir)
+        erp.plot_spectral_topo(subject_id, input_dir, output_dir)
+        print(f'================ Subject {subject_id} done ================')
+
+def grand_average_topo(input_dir, output_dir, exclude_subjects=False, excluded_subjects_list=[], population=None):
+
+    if exclude_subjects == True:
+
+        erp.plot_erp_topo(subject_id='GA', input_dir=input_dir, output_dir=output_dir, exclude_subjects=True, excluded_subjects_list=excluded_subjects_list, population=population)
+        erp.plot_spectral_topo(subject_id='GA', input_dir=input_dir, output_dir=output_dir, exclude_subjects=True, excluded_subjects_list=excluded_subjects_list, population=population)
+
+    else:
+
+        erp.plot_erp_topo(subject_id='GA', input_dir=input_dir, output_dir=output_dir, exclude_subjects=False, excluded_subjects_list=[], population=population)
+        erp.plot_spectral_topo(subject_id='GA', input_dir=input_dir, output_dir=output_dir, exclude_subjects=False, excluded_subjects_list=[], population=population)
+
+
+
+if __name__ == '__main__':
+
+
+    # Create an ArgumentParser object to handle command-line arguments
+    parser = argparse.ArgumentParser(description="Run specific functions based on user input")
+    parser.add_argument("mode", choices=["single", "GA"], help="Choose 'single' for single subjects or 'GA' for Grand Average")
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    if args.mode == "GA":
+        # Prompt the user to enter a two-digit number for the single subject
+        yes_or_no = input("Do you want to exclude subjects? ('yes' or 'no'): ")
+        if yes_or_no == 'yes':
+            grand_average_topo(input_dir, output_dir, exclude_subjects=True, excluded_subjects_list=excluded_subjects_list, population=population)
+        if yes_or_no == 'no':
+            grand_average_topo(input_dir, output_dir, exclude_subjects=False, excluded_subjects_list=[], population=population)
+        # Call the single subject function with the provided input
+        
+        print()
+    elif args.mode == "single":
+
+        loop_over_subjects_topo(subject_list, input_dir, output_dir)
+        print('================ All subjects done ================')
+
+    else:
+        print("Invalid mode. Please choose 'single' or 'GA'.")
