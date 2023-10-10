378ff0c78f9f62d66f2b4cf7fac1cdf4bed0d3cb
NicolasPiron <ncls.piron@gmail.com>
2023-10-10 11:47:10 +0200

    to average the resting state signal into a 2s evoked object

---
+++ EEG/resting-state/to_average.py
@@ -0,0 +1,44 @@
+import mne
+import os
+import glob
+
+# This file computes the average of the epochs for each subject and each condition (eyes closed and eyes open)
+#####################################################################
+# parameters to be changed by user
+
+# path to the derivative folder 
+input_dir = ''
+
+# path to the output folder
+output_dir = ''
+
+#####################################################################
+
+# list of subjects
+
+def to_average(input_dir, output_dir):
+
+    subjects_dirs = glob.glob(os.path.join(input_dir, 'sub-*'))
+
+    # for each condition
+    for condition in ['RESTINGSTATEOPEN', 'RESTINGSTATECLOSED']:
+
+        # loop over subjects and create the average
+        for dir in subjects_dirs:
+
+            # path to epochs
+            epochs_path = glob.glob(os.path.join(dir, condition, 'cleaned_epochs', 'sub-*.fif'))[0]
+            epochs = mne.read_epochs(epochs_path)
+
+            # create the average
+            average = epochs.average()
+
+            # save the average
+            subject = os.path.basename(dir)
+            if not os.path.join(output_dir, subject, condition, 'average'):
+                os.makedirs(os.path.join(output_dir, subject, condition, 'average'))
+            average.save(os.path.join(output_dir, subject, condition, 'average', f'{subject}_{condition}_average-epo.fif'))
+
+if __name__ == '__main__':
+    to_average(input_dir, output_dir)
+
