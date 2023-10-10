ef6b242748ddf238fd86850235765f003f614eee
Nicolas Piron <ncls.piron@gmail.com>
2023-10-10 17:04:06 +0200

    fixed bug

--- EEG/resting-state/to_average.py
+++ EEG/resting-state/to_average.py
@@ -7,10 +7,10 @@ import glob
 # parameters to be changed by user
 
 # path to the derivative folder 
-input_dir = ''
+input_dir = '/home/nicolasp/shared_PULSATION/derivative'
 
 # path to the output folder
-output_dir = ''
+output_dir = '/home/nicolasp/shared_PULSATION/derivative'
 
 #####################################################################
 
@@ -20,29 +20,28 @@ def to_average(input_dir, output_dir):
     subjects_dirs = glob.glob(os.path.join(input_dir, 'sub-*'))
 
     # for each condition
-    for condition in ['RESTINGSTATEOPEN', 'RESTINGSTATECLOSED']:
+    for condition in ['RESTINGSTATEOPEN', 'RESTINGSTATECLOSE']:
 
         # loop over subjects and create the average
         for dir in subjects_dirs:
 
             subject = os.path.basename(dir)
-            # path to epochs
-            epochs_path = glob.glob(os.path.join(dir, condition, 'cleaned_epochs', 'sub-*.fif'))[0]
 
-            # check id the file exists
-            if os.path.exists(epochs_path):
+            try:
+                epochs_path = glob.glob(os.path.join(dir, condition, 'cleaned_epochs', 'sub-*.fif'))[0]
                 epochs = mne.read_epochs(epochs_path)
-
+                print('========== epochs found')
                 # create the average
                 average = epochs.average()
-
+                print('========== evoked object successfuly created')
+                
                 # save the average
-                if not os.path.join(output_dir, subject, condition, 'average'):
+                if not os.path.exists(os.path.join(output_dir, subject, condition, 'average')):
                     os.makedirs(os.path.join(output_dir, subject, condition, 'average'))
-                average.save(os.path.join(output_dir, subject, condition, 'average', f'{subject}_{condition}_average-epo.fif'))
+                average.save(os.path.join(output_dir, subject, condition, 'average', f'{subject}_{condition}_ave.fif'))
 
                 print(f'========== Average {condition} computed for subject {subject}')
-            else:
+            except:
                 print(f'========== No epochs found for subject {subject}, condition {condition}')
 
 if __name__ == '__main__':
