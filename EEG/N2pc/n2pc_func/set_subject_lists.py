44f22b019f0a1275def28282e07d26dae201e258
NicolasPiron <ncls.piron@gmail.com>
2023-11-13 19:13:30 +0100

    update subject_list

--- EEG/N2pc/n2pc_func/set_subject_lists.py
+++ EEG/N2pc/n2pc_func/set_subject_lists.py
@@ -2,12 +2,13 @@ def get_subject_list():
     
     print('===== subject list was set automatically =====')
     print('===== see set_subject_lists.py =====')
-    subject_list = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 51, 52, 53, 54, 55, 56, 57, 58, 59, 70, 71, 72, 73]
+    subject_list = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20,
+                     21, 22, 23, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 70, 71, 72, 73, 74, 75, 76, 77, 78]
     return subject_list
 
 def get_excluded_subjects_list():
 
     print('===== excluded subject list was set automatically =====')
     print('===== see set_subject_lists.py =====')
-    excluded_subjects_list = [8, 9, 10, 11, 15]
+    excluded_subjects_list = [8, 9, 10, 11, 14, 15, 57]
     return excluded_subjects_list
