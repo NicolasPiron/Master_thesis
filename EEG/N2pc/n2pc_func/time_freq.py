ca09fb34295da4128ccc39ae5536496d040ae78c
NicolasPiron <ncls.piron@gmail.com>
2023-12-18 17:19:47 +0100

    added channels PO3-4

--- EEG/N2pc/n2pc_func/time_freq.py
+++ EEG/N2pc/n2pc_func/time_freq.py
@@ -193,8 +193,8 @@ def plot_tfr_population(input_dir, output_dir, subject_list, population):
                                topomap_args={'vlim': (0,0.0000000015)},
                                title=f'{population} - {condition}', colorbar=True, show=False)
         fig2 = tfr.plot_topo(title=f'{population} - {condition}', tmin=0, tmax=0.6, fmin=8, fmax=30, vmin=0, vmax=0.0000000005, show=False)
-        fig3 = tfr.plot(picks=['P7', 'PO7', 'P9', 'O1'], combine='mean', title=f'{population} - {condition} - occip left', tmin=0, fmin=8, fmax=30, vmin=0, vmax=0.0000000009, show=False)[0]
-        fig4 = tfr.plot(picks=['P8', 'PO8', 'P10', 'O2'], combine='mean', title=f'{population} - {condition} - occip right', tmin=0, fmin=8, fmax=30, vmin=0, vmax=0.0000000009, show=False)[0]
+        fig3 = tfr.plot(picks=['PO3', 'P7', 'PO7', 'P9', 'O1'], combine='mean', title=f'{population} - {condition} - occip left', tmin=0, fmin=8, fmax=30, vmin=0, vmax=0.0000000009, show=False)[0]
+        fig4 = tfr.plot(picks=['PO4', 'P8', 'PO8', 'P10', 'O2'], combine='mean', title=f'{population} - {condition} - occip right', tmin=0, fmin=8, fmax=30, vmin=0, vmax=0.0000000009, show=False)[0]
         fig1.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'joint', population, f'{population}-{condition}-joint-tfr-plt.png'))
         fig2.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'topo', population, f'{population}-{condition}-topo-plot.png'))
         fig3.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'occip', population, f'{population}-{condition}-occip-left.png'))
