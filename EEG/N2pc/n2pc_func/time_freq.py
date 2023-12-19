9d161134d0085cffb948029ebc465054a7cba35c
NicolasPiron <ncls.piron@gmail.com>
2023-12-19 09:11:21 +0100

    occip 2 sides plot

--- EEG/N2pc/n2pc_func/time_freq.py
+++ EEG/N2pc/n2pc_func/time_freq.py
@@ -166,11 +166,14 @@ def plot_tfr_single_subj(subject_id, input_dir, output_dir):
         fig3 = tfr.plot(picks=['PO3', 'P7', 'PO7', 'P9', 'O1'], combine='mean', title=f'{subject_id} - {condition} - occip left', tmin=0, fmin=8, fmax=30, vmin=0, show=False)[0]
         fig4 = tfr.plot(picks=['PO4', 'P8', 'PO8', 'P10', 'O2'], combine='mean', title=f'{subject_id} - {condition} - occip right', tmin=0, fmin=8, fmax=30, vmin=0, show=False)[0]
         fig5 = tfr.plot_joint(timefreqs=((0.1, 18),(0.3,18),(0.5,18)), tmin=0, tmax=0.6, title=f'sub-{subject_id} - {condition}', colorbar=True, show=False)
+        fig6 = tfr.plot(picks=['PO3', 'P7', 'PO7', 'P9', 'O1',
+                                 'PO4', 'P8', 'PO8', 'P10', 'O2'], combine='mean', title=f'{subject_id} - {condition} - occip', tmin=0, fmin=8, fmax=30, vmin=0, show=False)[0]
         fig1.savefig(os.path.join(output_dir, f'sub-{subject_id}', 'N2pc', 'time_freq', 'tfr-plots', 'joint', f'sub-{subject_id}-{condition}-joint-tfr-plt.png'))
         fig2.savefig(os.path.join(output_dir, f'sub-{subject_id}', 'N2pc', 'time_freq', 'tfr-plots', 'topo', f'sub-{subject_id}-{condition}-topo-plot.png'))
         fig3.savefig(os.path.join(output_dir, f'sub-{subject_id}', 'N2pc', 'time_freq', 'tfr-plots', 'occip', f'sub-{subject_id}-{condition}-occip-left.png'))
         fig4.savefig(os.path.join(output_dir, f'sub-{subject_id}', 'N2pc', 'time_freq', 'tfr-plots', 'occip', f'sub-{subject_id}-{condition}-occip-right.png'))
         fig5.savefig(os.path.join(output_dir, f'sub-{subject_id}', 'N2pc', 'time_freq', 'tfr-plots', 'joint', f'sub-{subject_id}-{condition}-joint-tfr-plt-beta.png'))
+        fig6.savefig(os.path.join(output_dir, f'sub-{subject_id}', 'N2pc', 'time_freq', 'tfr-plots', 'occip', f'sub-{subject_id}-{condition}-occip.png'))
         plt.close('all')
         print(f'================= {condition} plots done for {subject_id}')
     
@@ -206,11 +209,14 @@ def plot_tfr_population(input_dir, output_dir, subject_list, population):
         fig2 = tfr.plot_topo(title=f'{population} - {condition}', tmin=0, tmax=0.6, fmin=8, fmax=30, vmin=0, vmax=0.0000000005, show=False)
         fig3 = tfr.plot(picks=['PO3', 'P7', 'PO7', 'P9', 'O1'], combine='mean', title=f'{population} - {condition} - occip left', tmin=0, fmin=8, fmax=30, vmin=0, vmax=0.0000000009, show=False)[0]
         fig4 = tfr.plot(picks=['PO4', 'P8', 'PO8', 'P10', 'O2'], combine='mean', title=f'{population} - {condition} - occip right', tmin=0, fmin=8, fmax=30, vmin=0, vmax=0.0000000009, show=False)[0]
+        fig6 = tfr.plot(picks=['PO3', 'P7', 'PO7', 'P9', 'O1',
+                               'PO4', 'P8', 'PO8', 'P10', 'O2'], combine='mean', title=f'{population} - {condition} - occip', tmin=0, fmin=8, fmax=30, vmin=0, vmax=0.0000000009, show=False)[0]
         fig1.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'joint', population, f'{population}-{condition}-joint-tfr-plt.png'))
         fig2.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'topo', population, f'{population}-{condition}-topo-plot.png'))
         fig3.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'occip', population, f'{population}-{condition}-occip-left.png'))
         fig4.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'occip', population, f'{population}-{condition}-occip-right.png'))
         fig5.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'joint', population, f'{population}-{condition}-joint-tfr-plt-beta.png'))
+        fig6.savefig(os.path.join(output_dir, 'all_subj', 'N2pc', 'time_freq', 'tfr-plots', 'occip', population, f'{population}-{condition}-occip.png'))
         plt.close('all')
         print(f'================= {condition} plots done for {population}')
 
