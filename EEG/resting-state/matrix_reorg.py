e4e6f4506da81a99dad4f68cb1af7ae6c3b7bb38
NicolasPiron <ncls.piron@gmail.com>
2023-12-20 14:00:16 +0100

    reorg full grp matrices

--- EEG/resting-state/matrix_reorg.py
+++ EEG/resting-state/matrix_reorg.py
@@ -69,6 +69,46 @@ def main():
             matrix = reorder_matrix(matrix, channel_order)
             plot_and_save_reorg_matrix(matrix, mat_name, os.path.join(input_dir, mat_dir))
 
+
+##########################################################################################
+# for full matrices
+            
+
+def find_matrices(mat_dir):
+    
+    matrices = glob.glob(mat_dir + '/*-plv-*csv')
+    mat_dict = {}
+    for mat in matrices:
+        mat_name = mat.split('/')[-1]
+        mat_dict[mat_name] = pd.read_csv(mat, index_col=0)
+        print(mat_name)
+    print(f'============= matrices in {mat_dir} =============')
+    
+    return mat_dict
+
+def main2():
+
+    input_dir, _ = get_paths()
+    input_dir = os.path.join(input_dir, 'all_subj', 'resting-state', 'connectivity', 'static')
+
+    mat_dirs = ['old_control', 'young_control', 'thal_control', 'pulvinar']
+
+    channel_order = ['Fp1', 'AF7', 'AF3', 'F1', 'F3', 'F5', 'F7', 'FT7', 'FC5', 'FC3','FC1', 'C1',
+                 'C3', 'C5', 'T7', 'TP7', 'CP5', 'CP3', 'CP1', 'P1', 'P3', 'P5', 'P7', 'P9', 'PO7',
+                 'PO3','O1', 'Fpz', 'AFz', 'Fz', 'FCz', 'Cz', 'CPz', 'Pz','POz', 'Oz','Iz',
+                 'Fp2','AF8','AF4', 'F2', 'F4', 'F6', 'F8', 'FT8', 'FC6', 'FC4', 'FC2', 'C2', 'C4',
+                 'C6', 'T8', 'TP8', 'CP6', 'CP4', 'CP2', 'P2', 'P4', 'P6', 'P8', 'P10', 'PO8', 'PO4', 'O2']
+    
+    for dir in mat_dirs:
+        print(dir)
+        mat_dict = find_matrices(os.path.join(input_dir, dir, 'conn_data'))
+        for mat_name, matrix in mat_dict.items():
+            print('lol')
+            matrix = reorder_matrix(matrix, channel_order)
+            if not os.path.exists(os.path.join(input_dir, dir, 'conn_data', 'reorg')):
+                os.makedirs(os.path.join(input_dir, dir, 'conn_data', 'reorg'))
+            plot_and_save_reorg_matrix(matrix, mat_name, os.path.join(input_dir, dir, 'conn_data', 'reorg'))
     
 if __name__ == '__main__':
-    main()
+    #main()
+    main2()
