8af61eaf8116b309b211355156a34dfdb4c55f24
NicolasPiron <ncls.piron@gmail.com>
2024-03-04 10:50:54 +0100

    static conn computing with last young subjects

--- EEG/resting-state/plv_static_matrices.py
+++ EEG/resting-state/plv_static_matrices.py
@@ -7,16 +7,16 @@ import numpy as np
 input_dir, output_dir = get_paths()
 subject_list = get_subject_list()
 
-pulvinar = [51, 53, 59]
+pulvinar = [51, 53, 59, 60]
 old_control = [sub for sub in subject_list if sub < 50]
 young_control = [sub for sub in subject_list if sub > 69]
 stroke = [sub for sub in subject_list if sub > 49 and sub < 70]
 thal_control = [sub for sub in stroke if sub not in pulvinar]
 
-group_dict = {'old_control':old_control, 'young_control':young_control, 'stroke':stroke, 'thal_control':thal_control, 'pulvinar':pulvinar}
+group_dict = {'old_control':old_control, 'young_control':young_control, 'thal_control':thal_control, 'pulvinar':pulvinar}
 
 # Define parameters
-metric_list = ['coh', 'plv', 'ciplv', 'pli', 'wpli']
+metric_list = ['plv', 'pli']
 freqs_list = [np.arange(4,9), np.arange(8, 13), np.arange(12, 17), np.arange(16, 31)]
 
 if __name__ == '__main__':
@@ -26,21 +26,22 @@ if __name__ == '__main__':
         for metric in metric_list:
             for freqs in freqs_list:
                 try:
+                    print(f'Creating matrix for subject {subject}, {metric}, {freqs}')
                     static_connectivity.create_conn_matrix_subject(subject_id, metric, freqs, input_dir, output_dir)
                 except:
                     print(f'Could not create matrix for subject {subject}, {metric}, {freqs}')
                     continue
 
-    for group_name, group_list in group_dict.items():
-        group_list = sorted(group_list)
-        for metric in metric_list:
-            for freqs in freqs_list:
-                try:
-                    df_open_group, df_closed_group = static_connectivity.create_conn_matrix_group(group_list, metric, freqs, input_dir, output_dir)
-                    static_connectivity.plot_and_save_group_matrix(df_open_group, df_closed_group, group_name, metric, freqs, output_dir)
-                except:
-                    print(f'Could not create matrix for {group_name}, {metric}, {freqs}')
-                    continue
+    #for group_name, group_list in group_dict.items():
+    #    group_list = sorted(group_list)
+    #    for metric in metric_list:
+    #        for freqs in freqs_list:
+    #            try:
+    #                df_open_group, df_closed_group = static_connectivity.create_conn_matrix_group(group_list, metric, freqs, input_dir, output_dir)
+    #                static_connectivity.plot_and_save_group_matrix(df_open_group, df_closed_group, group_name, metric, freqs, output_dir)
+    #            except:
+    #                print(f'Could not create matrix for {group_name}, {metric}, {freqs}')
+    #                continue
 
 
 
--- EEG/resting-state/resting_func/set_subject_lists.py
+++ EEG/resting-state/resting_func/set_subject_lists.py
@@ -2,7 +2,8 @@ def get_subject_list():
     
     print('===== subject list was set automatically =====')
     print('===== see set_subject_lists.py =====')
-    subject_list = [1, 2, 3, 4, 6, 7, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 51, 52, 53, 54, 55, 56, 58, 59, 60, 70, 71, 72, 73, 74, 75, 76, 77, 78]
+    subject_list = [1, 2, 3, 4, 6, 7, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 51, 52, 53,
+                     54, 55, 56, 58, 59, 60, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87]
     return subject_list
 
 def get_excluded_subjects_list():
