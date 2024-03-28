70274adbd1a68c3d6cd4085881fc5a021b1ef27a
NicolasPiron <ncls.piron@gmail.com>
2024-03-28 09:46:19 +0100

    changed subj ids as strings so recognized for side invertion

--- EEG/resting-state/src_static_conn.py
+++ EEG/resting-state/src_static_conn.py
@@ -6,7 +6,11 @@ import numpy as np
 input_dir, output_dir = get_paths()
 subject_list = get_subject_list()
 
-right_lesion = [51, 53, 54, 58, 59]
+pulvinar = ['51', '53', '59', '60']
+old_control = [sub for sub in subject_list if sub < 50]
+thal_control = ['52', '54', '55', '56', '58']
+
+right_lesion = ['51', '53', '54', '58', '59']
 metric = 'ciplv'
 freqs = [np.arange(4,9), np.arange(8, 13), np.arange(12, 17), np.arange(16, 31)]
 
@@ -24,4 +28,5 @@ for subject_id in subject_list:
             except:
                 print(f'Error in {subject_id} - {condition} - {freq}')
                 continue
+
 
