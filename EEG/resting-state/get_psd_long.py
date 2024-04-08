b642b4032352a58e0574eebbe8a9b6ed77b8e1ae
NicolasPiron <ncls.piron@gmail.com>
2024-04-08 16:46:04 +0200

    Add get_psd_long.py script for calculating power spectral density (PSD) for resting state EEG data

---
+++ EEG/resting-state/get_psd_long.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+import pandas as pd
+from resting_func.set_paths import get_paths
+from resting_func.set_subject_lists import get_subject_list
+from resting_func.rs_frequency import get_data, get_roi_dict
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def get_long_subject(subject_id):
+
+    df_list = []
+    roi_dict = get_roi_dict()
+    conditions = ['RESTINGSTATEOPEN', 'RESTINGSTATECLOSE']
+
+    for condition in conditions:
+
+        if condition == 'RESTINGSTATEOPEN':
+            cond_name = 'open'
+        else:
+            cond_name = 'closed'
+
+        spec, freq = get_data(subject_id, condition)
+
+        data_dict = {}
+        for roi in roi_dict.keys():
+            data_dict[roi] = spec[roi_dict[roi]].mean(axis=0)
+
+        df = pd.DataFrame(data_dict)
+        df['freq'] = freq
+        df['condition'] = cond_name
+        df['ID'] = subject_id
+        df_long = pd.melt(df, id_vars=['ID', 'condition', 'freq'], var_name='ROI', value_name='PSD')
+        df_list.append(df_long)
+
+    return pd.concat(df_list, axis=0)
+
+def get_long_all_subjects(subject_list):
+
+    _, o = get_paths()
+    path = os.path.join(o, 'all_subj', 'resting-state', 'power', 'df')
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    df_list = []
+
+    for subject in subject_list:
+        try:
+            df = get_long_subject(subject)
+            df_list.append(df)
+        except:
+            print(f'========== Error with subject {subject} ==========')
+
+    df = pd.concat(df_list, axis=0)
+    df.to_csv(os.path.join(path, 'long_psd.csv'), index=False)
+
+    return df
+
+if __name__ == '__main__':
+
+    subject_list = get_subject_list()
+    df = get_long_all_subjects(subject_list)
+    print('========== Done ==========')
+    print(f'subjects in df : {df['ID'].unique()}')
+
+
+
