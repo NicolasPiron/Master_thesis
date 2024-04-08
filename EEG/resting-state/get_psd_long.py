7c0859d265abfee8fe80f0b064c4aded18f7c7be
NicolasPiron <ncls.piron@gmail.com>
2024-04-08 17:06:56 +0200

    Add swap_sides parameter to get_long_subject() function

--- EEG/resting-state/get_psd_long.py
+++ EEG/resting-state/get_psd_long.py
@@ -7,7 +7,7 @@ from resting_func.rs_frequency import get_data, get_roi_dict
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def get_long_subject(subject_id):
+def get_long_subject(subject_id, swap_sides=False):
 
     df_list = []
     roi_dict = get_roi_dict()
@@ -33,9 +33,24 @@ def get_long_subject(subject_id):
         df_long = pd.melt(df, id_vars=['ID', 'condition', 'freq'], var_name='ROI', value_name='PSD')
         df_list.append(df_long)
 
-    return pd.concat(df_list, axis=0)
+    df = pd.concat(df_list, axis=0)
+
+    def swap_suffix(roi):
+        if roi.endswith('_l'):
+            return roi[:-2] + '_r'
+        elif roi.endswith('_r'):
+            return roi[:-2] + '_l'
+        return roi
+
+    if swap_sides:
+
+        df['ROI'] = df['ROI'].apply(swap_suffix)
+
+    return df
 
 def get_long_all_subjects(subject_list):
+
+    to_be_swapped = [51, 53, 54, 58, 59]
 
     _, o = get_paths()
     path = os.path.join(o, 'all_subj', 'resting-state', 'power', 'df')
@@ -45,14 +60,18 @@ def get_long_all_subjects(subject_list):
     df_list = []
 
     for subject in subject_list:
+        if subject in to_be_swapped:
+            swap_sides = True
+        else:
+            swap_sides = False
         try:
-            df = get_long_subject(subject)
+            df = get_long_subject(subject, swap_sides=swap_sides)
             df_list.append(df)
         except:
             print(f'========== Error with subject {subject} ==========')
 
     df = pd.concat(df_list, axis=0)
-    df.to_csv(os.path.join(path, 'long_psd.csv'), index=False)
+    df.to_csv(os.path.join(path, 'long_psd_corrected.csv'), index=False)
 
     return df
 
