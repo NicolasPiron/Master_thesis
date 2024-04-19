962a095f1ad5fbe1b79112145b71b4bca5a542a8
NicolasPiron <ncls.piron@gmail.com>
2024-04-19 18:40:21 +0200

    Update invert_subjects list in get_n2pc_array_group function

--- EEG/N2pc/stats.py
+++ EEG/N2pc/stats.py
@@ -64,7 +64,7 @@ def get_n2pc_array_subject(subject_id, side=None, invert=False):
 
 def get_n2pc_array_group(subject_list, side=None):
 
-    invert_subjects = ['52', '54', '55', '56', '58']
+    invert_subjects = ['52', '55', '56', '60']
     array_list = []
     for subject_id in subject_list:
         if subject_id in invert_subjects:
