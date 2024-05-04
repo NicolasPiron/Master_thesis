2cc23a261d20e18780a73da275638462afc4529f
NicolasPiron <ncls.piron@gmail.com>
2024-05-04 16:39:20 +0200

    Refactor plv_static_matrices.py to create group matrices and plots

--- EEG/resting-state/plv_static_matrices.py
+++ EEG/resting-state/plv_static_matrices.py
@@ -20,30 +20,30 @@ freqs_list = [np.arange(4,9), np.arange(8, 13), np.arange(12, 17), np.arange(16,
 
 if __name__ == '__main__':
 
-    for subject_id in subject_list:
-        if subject_id in ['52', '55', '56', '60']:
-            invert_sides = True
-        else:
-            invert_sides = False
-        for metric in metric_list:
-            for freqs in freqs_list:
-                try:
-                    print(f'Creating matrix for subject {subject_id}, {metric}, {freqs}')
-                    static_connectivity.create_conn_matrix_subject(subject_id, metric, freqs, input_dir, output_dir, invert_sides=invert_sides)
-                except:
-                    print(f'Could not create matrix for subject {subject_id}, {metric}, {freqs}')
-                    continue
+    # for subject_id in subject_list:
+    #     if subject_id in ['52', '55', '56', '60']:
+    #         invert_sides = True
+    #     else:
+    #         invert_sides = False
+    #     for metric in metric_list:
+    #         for freqs in freqs_list:
+    #             try:
+    #                 print(f'Creating matrix for subject {subject_id}, {metric}, {freqs}')
+    #                 static_connectivity.create_conn_matrix_subject(subject_id, metric, freqs, input_dir, output_dir, invert_sides=invert_sides)
+    #             except:
+    #                 print(f'Could not create matrix for subject {subject_id}, {metric}, {freqs}')
+    #                 continue
 
-    # for group_name, group_list in group_dict.items():
-    #    group_list = sorted(group_list)
-    #    for metric in metric_list:
-    #        for freqs in freqs_list:
-    #            try:
-    #                df_open_group, df_closed_group = static_connectivity.create_conn_matrix_group(group_list, metric, freqs, input_dir, output_dir, source=True)
-    #                static_connectivity.plot_and_save_group_matrix(df_open_group, df_closed_group, group_name, metric, freqs, output_dir, source=True)
-    #            except:
-    #                print(f'Could not create matrix for {group_name}, {metric}, {freqs}')
-    #                continue
+    for group_name, group_list in group_dict.items():
+       group_list = sorted(group_list)
+       for metric in metric_list:
+           for freqs in freqs_list:
+               try:
+                   df_open_group, df_closed_group = static_connectivity.create_conn_matrix_group(group_list, metric, freqs, input_dir, output_dir, source=False)
+                   static_connectivity.plot_and_save_group_matrix(df_open_group, df_closed_group, group_name, metric, freqs, output_dir, source=False)
+               except:
+                   print(f'Could not create matrix for {group_name}, {metric}, {freqs}')
+                   continue
 
 
 
