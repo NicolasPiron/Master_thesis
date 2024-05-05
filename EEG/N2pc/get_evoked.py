ba6a68335747b9f4191ff57ebe33804472587bd9
NicolasPiron <ncls.piron@gmail.com>
2024-05-05 16:16:46 +0200

    remove unused task parameter in get_evoked.py

--- EEG/N2pc/get_evoked.py
+++ EEG/N2pc/get_evoked.py
@@ -13,18 +13,18 @@ subject_list = get_subject_list()
 
 
 
-def loop_for_evoked(subject_list, task, input_dir):
+def loop_for_evoked(subject_list, input_dir):
 
     for subject in subject_list:
         subject_id = str(subject)
         if len(subject_id) == 1:
             subject_id = '0' + subject_id
         try:
-            erp.to_evoked(subject_id, task, input_dir)
+            erp.to_evoked(subject_id, input_dir)
         except:
             print('Error with subject ' + subject_id)
             continue
 
 if __name__ == '__main__':
 
-    loop_for_evoked(subject_list, task, input_dir)
+    loop_for_evoked(subject_list, input_dir)
